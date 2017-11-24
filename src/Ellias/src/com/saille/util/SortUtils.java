1cff650cfffb8e0c9457da788c1ca6ca5d4b135f
heqinyun <heqinyun@huaan.com.cn>
2017-11-24 17:29:55 +0800

    add quick sort demo

--- src/Ellias/src/com/saille/util/SortUtils.java
+++ src/Ellias/src/com/saille/util/SortUtils.java
@@ -5,162 +5,42 @@ import java.util.List;
 import java.util.Map;
 
 public class SortUtils {
-    public static final int SORT_JIAGE = 1;
+    //快速排序demo,low=0, high=a.length - 1
+    public static void quickSortDemo(int[] a, int low, int high) {
+        int start = low;
+        int end = high;
+        int key = a[low];
 
-    /*
-    public static List<Gangkou> sortGangkou(List<Gangkou> list, int start, int end) {
-        if(start >= end) {
-            return list;
-        }
-        int pos = start;
-        for(int i = pos + 1; i < end; i++) {
-            boolean needSwap = false;
-            if(((Gangkou) list.get(i)).getPinyin().compareTo(((Gangkou) list.get(pos)).getPinyin()) < 0) {
-                needSwap = true;
-            } else if(((Gangkou) list.get(i)).getPinyin().compareTo(((Gangkou) list.get(pos)).getPinyin()) > 0) {
-                needSwap = false;
-            } else
-            if(Collator.getInstance(Locale.CHINESE).compare(((Gangkou) list.get(i)).getName(), ((Gangkou) list.get(pos)).getName()) < 0) {
-                needSwap = true;
+        while(end > start) {
+            //从后往前比较
+            while(end > start && a[end] >= key) { //如果没有比关键值小的，比较下一个，直到有比关键值小的交换位置，然后又从前往后比较
+                end--;
             }
-
-            if(needSwap) {
-                Gangkou tmp = (Gangkou) list.get(i);
-                for(int m = i; m > pos; m--) {
-                    list.set(m, list.get(m - 1));
-                }
-                list.set(pos, tmp);
+            if(a[end] <= key) {
+                int temp = a[end];
+                a[end] = a[start];
+                a[start] = temp;
             }
-            pos = i;
+            //从前往后比较
+            while(end > start && a[start] <= key) { //如果没有比关键值大的，比较下一个，直到有比关键值大的交换位置
+                start++;
+            }
+            if(a[start] >= key) {
+                int temp = a[start];
+                a[start] = a[end];
+                a[end] = temp;
+            }
+            //此时第一次循环比较结束，关键值的位置已经确定了。左边的值都比关键值小，右边的值都比关键值大，但是两边的顺序还有可能是不一样的，进行下面的递归调用
         }
-        sortGangkou(list, start, pos);
-        sortGangkou(list, pos + 1, end);
-        return list;
+        //递归
+        int count1 = 1, count2 = 1;
+        if(start > low) {
+            quickSortDemo(a, low, start - 1);//左边序列。第一个索引位置到关键值索引-1
+        }
+        if(end < high) {
+            quickSortDemo(a, end + 1, high);//右边序列。从关键值索引+1到最后一个
+        }
     }
-    */
-
-    /*
-    public static List<Guojia> sortGuojia(List<Guojia> list, int start, int end) {
-        if(start >= end) {
-            return list;
-        }
-        int pos = start;
-        for(int i = pos + 1; i < end; i++) {
-            boolean needSwap = false;
-            if(((Guojia) list.get(i)).getPinyin().compareTo(((Guojia) list.get(pos)).getPinyin()) < 0) {
-                needSwap = true;
-            } else if(((Guojia) list.get(i)).getPinyin().compareTo(((Guojia) list.get(pos)).getPinyin()) > 0) {
-                needSwap = false;
-            } else
-            if(Collator.getInstance(Locale.CHINESE).compare(((Guojia) list.get(i)).getName(), ((Guojia) list.get(pos)).getName()) < 0) {
-                needSwap = true;
-            }
-
-            if(needSwap) {
-                Guojia tmp = (Guojia) list.get(i);
-                for(int m = i; m > pos; m--) {
-                    list.set(m, list.get(m - 1));
-                }
-                list.set(pos, tmp);
-            }
-            pos = i;
-        }
-        sortGuojia(list, start, pos);
-        sortGuojia(list, pos + 1, end);
-        return list;
-    }
-    */
-
-    /*
-    public static List<Shangpin> sortShangpin(List<Shangpin> list, int start, int end) {
-        if(start >= end) {
-            return list;
-        }
-        int pos = start;
-        for(int i = pos + 1; i < end; i++) {
-            boolean needSwap = false;
-            if(((Shangpin) list.get(i)).getPinyin().compareTo(((Shangpin) list.get(pos)).getPinyin()) < 0) {
-                needSwap = true;
-            } else if(((Shangpin) list.get(i)).getPinyin().compareTo(((Shangpin) list.get(pos)).getPinyin()) > 0) {
-                needSwap = false;
-            } else
-            if(Collator.getInstance(Locale.CHINESE).compare(((Shangpin) list.get(i)).getName(), ((Shangpin) list.get(pos)).getName()) < 0) {
-                needSwap = true;
-            }
-
-            if(needSwap) {
-                Shangpin tmp = (Shangpin) list.get(i);
-                for(int m = i; m > pos; m--) {
-                    list.set(m, list.get(m - 1));
-                }
-                list.set(pos, tmp);
-            }
-            pos = i;
-        }
-        sortShangpin(list, start, pos);
-        sortShangpin(list, pos + 1, end);
-        return list;
-    }
-    */
-
-    /*
-    public static List<Leibie> sortLeibie(List<Leibie> list, int start, int end) {
-        if(start >= end) {
-            return list;
-        }
-        int pos = start;
-        for(int i = pos + 1; i < end; i++) {
-            boolean needSwap = false;
-            if(((Leibie) list.get(i)).getPinyin().compareTo(((Leibie) list.get(pos)).getPinyin()) < 0) {
-                needSwap = true;
-            } else if(((Leibie) list.get(i)).getPinyin().compareTo(((Leibie) list.get(pos)).getPinyin()) > 0) {
-                needSwap = false;
-            } else
-            if(Collator.getInstance(Locale.CHINESE).compare(((Leibie) list.get(i)).getName(), ((Leibie) list.get(pos)).getName()) < 0) {
-                needSwap = true;
-            }
-
-            if(needSwap) {
-                Leibie tmp = (Leibie) list.get(i);
-                for(int m = i; m > pos; m--) {
-                    list.set(m, list.get(m - 1));
-                }
-                list.set(pos, tmp);
-            }
-            pos = i;
-        }
-        sortLeibie(list, start, pos);
-        sortLeibie(list, pos + 1, end);
-        return list;
-    }
-    */
-
-    /*
-    public static List<Jiage> sortJiage(List<Jiage> list, int start, int end, int sort) {
-        if(start >= end) {
-            return list;
-        }
-        int pos = start;
-        for(int i = pos + 1; i < end; i++) {
-            boolean needSwap = false;
-            if((sort == 1) && (((Jiage) list.get(i)).getJiage() > ((Jiage) list.get(pos)).getJiage())) {
-                needSwap = true;
-            }
-
-            if(needSwap) {
-                Jiage tmp = (Jiage) list.get(i);
-                for(int m = i; m > pos; m--) {
-                    list.set(m, list.get(m - 1));
-                }
-                list.set(pos, tmp);
-            }
-            pos = i;
-        }
-        sortJiage(list, start, pos, sort);
-        sortJiage(list, pos + 1, end, sort);
-        return list;
-    }
-    */
 
     //欧冠足球，球员镶嵌时的可镶嵌球员清单
     public static List<String[]> sortXiangqianPlayer(List<String[]> list, int start, int end) {
