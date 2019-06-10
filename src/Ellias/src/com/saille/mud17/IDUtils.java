a661b7f200a959971a0c732695982139a0cd1ad9
unknown <H00672@HAFM.LOCAL>
2019-06-10 09:02:16 +0800

    add 17mud id query status

---
+++ src/Ellias/src/com/saille/mud17/IDUtils.java
@@ -0,0 +1,11 @@
+package com.saille.mud17;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Ellias
+ * Date: 2018-11-22
+ * Time: 11:06
+ * To change this template use File | Settings | File Templates.
+ */
+public class IDUtils {
+}
